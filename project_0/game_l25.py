716d1f489d7ef87f714067b4176b38b92eb679a8
Niko-NN <0229162@mail.ru>
2022-11-06 10:33:20 +0300

    add game_l25.py

---
+++ project_0/game_l25.py
@@ -0,0 +1,15 @@
+x = 75
+user_num = 0
+cnt = 0
+
+while True:
+    user_num = int(input('Я загадал число от 1 до 100 - угадай его: '))
+    cnt += 1
+    if user_num == x:
+        print(f'Ты угадал число за {cnt} попыток')
+        print('Спасибо за игру')
+        break
+    elif user_num > x:
+        print('Мое число меньше')
+    else:
+        print('Мое число больше')
